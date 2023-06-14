2fa27b2346178edeb082e528cddb62a2bde3aedd
IsabelaSouto <belasoutomaior@gmail.com>
2023-06-14 20:35:32 -0300

    requisito 1 finalizado

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,9 @@
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (ids) => {
-  // seu código aqui
-  // começando!!
-};
+// posso usar o .filter para as especies do id
+// posso usar o .some para retorne a espécie do animal referente a este id, se a função receber apenas um id
+
+const getSpeciesByIds = (...ids) =>
+  data.species.filter((idSpecies) => ids.some((id) => idSpecies.id === id));
 
 module.exports = getSpeciesByIds;

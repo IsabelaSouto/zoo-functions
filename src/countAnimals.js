6a233c6457912fad3f0d235b43d359c49c283b5c
IsabelaSouto <belasoutomaior@gmail.com>
2023-06-20 19:31:51 -0300

    requisito 5 finalizado

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
 const countAnimals = (animal) => {
-  // seu código aqui
+  if (!animal) {
+    const noParameter = {};
+    data.species.forEach((specie) => { noParameter[specie.name] = specie.residents.length; });
+    return noParameter;
+  }
+
+  const findSpecies = data.species.find((specie) => specie.name === animal.species);
+
+  if (animal.species && animal.sex) {
+    const filterSpecies = findSpecies.residents.filter((specie) => specie.sex === animal.sex);
+    return filterSpecies.length;
+  }
+
+  return findSpecies.residents.length;
 };
 
 module.exports = countAnimals;

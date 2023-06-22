93e27e9bd587d27c82843279f12ba688977faf36
IsabelaSouto <belasoutomaior@gmail.com>
2023-06-22 12:45:58 -0300

    requisito 8 finalizado

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,28 @@
 const data = require('../data/zoo_data');
 
 const countEntrants = (entrants) => {
-  // seu código aqui
+  const count = { child: 0, adult: 0, senior: 0 };
+
+  entrants.forEach((entrant) => {
+    if (entrant.age < 18) count.child += 1;
+    if (entrant.age >= 18 && entrant.age < 50) count.adult += 1;
+    if (entrant.age >= 50) count.senior += 1;
+  });
+  return count;
 };
 
 const calculateEntry = (entrants) => {
-  // seu código aqui
+  if (!entrants || entrants.length === 0) return 0;
+
+  const entrantCount = countEntrants(entrants);
+
+  const total = Object.entries(entrantCount)
+    .reduce((sum, [visit, count]) => {
+      const sumVisit = sum + count * data.prices[visit];
+      return sumVisit;
+    }, 0);
+
+  return total;
 };
 
 module.exports = { calculateEntry, countEntrants };

9a973a17a742a8d2c25bca71465e9ca5366cb32d
IsabelaSouto <belasoutomaior@gmail.com>
2023-06-26 13:06:10 -0300

    requisito 12 finalizado

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,31 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
-describe('Testes da função getOpeningHours', () => {});
+describe('Testes da função getOpeningHours', () => {
+  it('Para o teste não passando nenhum argumento, deve retornar o objeto', () => {
+    const object = {
+      Tuesday: { open: 8, close: 6 },
+      Wednesday: { open: 8, close: 6 },
+      Thursday: { open: 10, close: 8 },
+      Friday: { open: 10, close: 8 },
+      Saturday: { open: 8, close: 10 },
+      Sunday: { open: 8, close: 8 },
+      Monday: { open: 0, close: 0 },
+    };
+    expect(getOpeningHours()).toEqual(object);
+  });
+  it('Para os argumentos Monday e 09:00-AM deve retornar a string The zoo is closed', () => {
+    expect(getOpeningHours('Monday', '09:00-AM')).toBe('The zoo is closed');
+  });
+  it('Para os argumentos Tuesday e 09:00-AM deve retornar a string The zoo is open', () => {
+    expect(getOpeningHours('Tuesday', '09:00-AM')).toBe('The zoo is open');
+  });
+  it('Para os argumentos Wednesday e 09:00-PM deve retornar a string The zoo is closed', () => {
+    expect(getOpeningHours('Wednesday', '09:00-PM')).toBe('The zoo is closed');
+  });
+  it('Para quando uma string não contiver apenas dígitos de número deve lançar um erro', () => {
+    expect(() => getOpeningHours(5)).toThrow();
+  });
+//   it('Para quando uma string não contiver apenas dígitos de número deve lançar um erro The string should represent a number', () => {
+//     expect(() => getOpeningHours(5)).toThrow(new Error('The 5 should represent a number'));
+//   });
+});

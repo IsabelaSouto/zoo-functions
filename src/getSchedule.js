716ba9490252f275431c623a308fc2384a7b6540
IsabelaSouto <belasoutomaior@gmail.com>
2023-07-04 18:40:12 -0300

    requisito 9 finalizado

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,27 @@
 const data = require('../data/zoo_data');
 
-const getSchedule = (scheduleTarget) => {
-  // seu código aqui
-};
+const scheduleDay = (day) => data.species
+  .filter(({ availability }) => availability.includes(day))
+  .map(({ name }) => name);
 
+const getSchedule = (scheduleTarget) => {
+  const scheduleAnimal = data.species.find(({ name }) => name === scheduleTarget);
+  if (scheduleAnimal) return scheduleAnimal.availability;
+
+  const schedule = {};
+  Object.keys(data.hours).forEach((day) => {
+    if (day !== 'Monday') {
+      schedule[day] = {
+        officeHour: `Open from ${data.hours[day].open}am until ${data.hours[day].close}pm`,
+        exhibition: scheduleDay(day),
+      };
+    } else { schedule[day] = { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' }; }
+  });
+
+  if (Object.keys(data.hours).includes(scheduleTarget)) {
+    return { [scheduleTarget]: schedule[scheduleTarget] };
+  }
+
+  return schedule;
+};
 module.exports = getSchedule;
